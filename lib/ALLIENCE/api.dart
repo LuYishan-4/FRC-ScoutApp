61a6e61e538d7ed93be8d7c2a4c093486c8a4c90
user <wuming381906@gmail.com>
2026-02-22 10:32:12 +0800

    add new ip

--- lib/ALLIENCE/api.dart
+++ lib/ALLIENCE/api.dart
@@ -2,5 +2,5 @@ class Api {
 
   static String serverIp = "http://163.20.3.28:3000";//https://nonprofit-unchanneled-thea.ngrok-free.dev
 
-  static const String defaultIp = "http://163.20.3.28:3000";
+  static const String defaultIp = "http://163.20.3.28:3000";//now
 }
